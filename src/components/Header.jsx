b030befd489298f5dc6ed6c3781018fdb93f50e7
leila <mbiydzenyuyeileen@gmail.com>
2025-01-28 21:40:10 +0100

    adding project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  <div className="header">
+    <div className="site-logo">
+      MEL <span className="logo-name">QUIZ</span>
+    </div>
+    <nav>
+      <a href="Home"></a>
+      <a href="Quiz"></a>
+      <a href="Result"></a>
+    </nav>
+    <button className="btn"></button>
+  </div>;
+}
---
+++ src/components/footer.jsx
@@ -0,0 +1,39 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer-item1">
+          <ul className="home">
+            <h3>HOME</h3>
+            <a href="">Site brand</a>
+            <a href="">User interface</a>
+            <a href="">User friendly</a>
+            <a href="">Brand threat</a>
+            <a href="">Contact</a>
+          </ul>
+        </div>
+        <div className="footer-item2">
+          <ul className="quiz">
+            <h3>QUIZ</h3>
+            <a href="">Questions</a>
+            <a href="">Time Buzzer</a>
+            <a href="">Accessibilty</a>
+            <a href="">User interface</a>
+            <a href="">Navigation</a>
+          </ul>
+        </div>
+        <div className="footer-item3">
+          <ul className="result">
+            <h3>User score</h3>
+            <a href="">User profile</a>
+            <a href="">Quiz buzzer</a>
+            <a href="">Feedback</a>
+            <a href="">New publications</a>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
